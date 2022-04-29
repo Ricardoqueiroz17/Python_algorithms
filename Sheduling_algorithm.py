1a3b4328016536f958ba2236c51c58c80c14545c
Ricardoqueiroz17 <43968538+Ricardoqueiroz17@users.noreply.github.com>
2022-04-29 00:18:24 -0400

    Commiting greedy algorithm of optimum sheduling of activities

---
+++ Sheduling_algorithm.py
@@ -0,0 +1,21 @@
+#Greedy algorithm: select the optimum structure/schedule of activities that do not conflict the start time and finish time
+#The idea is to keep choosing the activities with the smallest finish time
+import numpy as np
+def greedy_algo(l):
+    #l is a list with elements indicating start and finish time
+    import numpy as np
+    sched = []
+    l = l[l[:,1].argsort()]
+    sched.append(l[0])
+    for i in range(1,len(l)):
+
+        if l[i][0] > sched[-1][1]:
+            sched.append(l[i])
+    return sched
+
+#Testing the algorithm
+l = np. array([[0,1],[2,3],[15,20],[4,7],[3,8],[2,8],[6,10],[10,13],[15,17]])
+greedy_algo(l)
+
+
+
